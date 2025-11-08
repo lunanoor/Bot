6e68c5d7e285c706950681a2099dbd9afbb45315
lunanoor <luminarycare402@gmail.com>
2025-11-08 19:00:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -22,3 +23,4 @@ def bot():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
